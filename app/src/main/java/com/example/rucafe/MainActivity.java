aedbf7d054825dbbdc8c115b7ac19e47520fbaaa
raymond <phillips.n.ray@gmail.com>
2022-04-24 13:20:51 -0400

    got fragment swtihcing to work, comment/remove scroller and spinner to build app for now

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
- * the main activity class that deals with swithching between the
+ * the main activity class that deals with switching between the
  * different fragment views of the program.
  *
  * @author Raymond Phillips, Xiaoxuan Chen
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Button coffeeButton, donutButton, orderingBasketButton, storeOrderButton;
+
     /**
      * a method to create the fragment instance on click of the button
      * @param savedInstanceState a Bundle object of the previous savedInstanceState
@@ -19,5 +32,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coffeeButton = findViewById(R.id.coffee);
+        donutButton = findViewById(R.id.donut);
+        orderingBasketButton = findViewById(R.id.ordering_basket);
+        storeOrderButton = findViewById(R.id.store_orders);
+
+        Spinner spinner = findViewById(R.id.coffeeSize);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.coffeeSizes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+        coffeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new coffee());
+            }
+        });
+
+        donutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new donut());
+            }
+        });
+
+        orderingBasketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new orderingBasket());
+            }
+        });
+
+        storeOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new storeOrders());
+            }
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment Fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment,Fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- app/src/main/java/com/example/rucafe/coffee.java
+++ app/src/main/java/com/example/rucafe/coffee.java
@@ -21,61 +21,8 @@ import android.widget.Toast;
  *
  * @author Raymond Phillips, Xiaoxuan Chen
  */
-public class coffee extends Fragment implements AdapterView.OnItemSelectedListener{
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public coffee() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment coffee.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static coffee newInstance(String param1, String param2) {
-        coffee fragment = new coffee();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    /**
-     * a method to create the fragment instance on click of the button
-     * @param savedInstanceState a Bundle object of the previous savedInstanceState
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Spinner s = (Spinner) getView().findViewById(R.id.coffeeSize);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.coffeeSizes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-        s.setOnItemSelectedListener(this);
-
-    }
+public class coffee extends Fragment{
+    View view;
 
     /**
      * a method to create the view of the fragment
@@ -88,38 +35,8 @@ public class coffee extends Fragment implements AdapterView.OnItemSelectedListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coffee, container, false);
+        view = inflater.inflate(R.layout.fragment_coffee, container, false);
+        return view;
     }
 
-    /**
-     * <p>Callback method to be invoked when an item in this view has been
-     * selected. This callback is invoked only when the newly selected
-     * position is different from the previously selected position or if
-     * there was no selected item.</p>
-     * <p>
-     * Implementers can call getItemAtPosition(position) if they need to access the
-     * data associated with the selected item.
-     *
-     * @param parent   The AdapterView where the selection happened
-     * @param view     The view within the AdapterView that was clicked
-     * @param position The position of the view in the adapter
-     * @param id       The row id of the item that is selected
-     */
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Callback method to be invoked when the selection disappears from this
-     * view. The selection can disappear for instance when touch is activated
-     * or when the adapter becomes empty.
-     *
-     * @param parent The AdapterView that now contains no selected item.
-     */
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
